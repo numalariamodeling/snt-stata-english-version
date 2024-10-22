d422142a14a2d3af3fb05cfdfb2a4f22ba7c6974
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-10-22 14:04:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -45,9 +45,9 @@ As SNT matures, more quality assurance is needed such that NMCPs can be confiden
             <div class="round-buttons">
                 <button class="rect-button" onclick="window.location.href='https://example.com/button1';">View R EN</button>
                 <button class="rect-button" onclick="window.location.href='https://example.com/button2';">View R FR</button>
-                <button class="rect-button" onclick="window.location.href='https://example.com/button3';">View R FR</button>
-                <button class="rect-button" onclick="window.location.href='https://example.com/button2';">View St FR</button>
-                <button class="rect-button" onclick="window.location.href='https://example.com/button3';">View St EN</button>
+                <button class="rect-button" onclick="window.location.href='https://example.com/button3';">View py EN</button>
+                <button class="rect-button" onclick="window.location.href='https://example.com/button2';">View py FR</button>
+                <button class="rect-button" onclick="window.location.href='https://example.com/button3';">View St FR</button>
             </div>
             
             <h4 id="stepByStep">Step-by-step guide</h4>
